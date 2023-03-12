70453da5d5afcd724cae956f3cd82e0605dc2468
Michael O'Connell <oconnellmj16@gmail.com>
2023-03-12 16:42:19 -0400

    add skybox plugin

--- src/level.rs
+++ src/level.rs
@@ -1,16 +1,21 @@
+mod skybox;
+
+use crate::level::skybox::SkyboxPlugin; // probably a better way to do this, in level for right now since nothing else needs to know about it yet
 use bevy::prelude::*;
 
 pub struct LevelPlugin;
 
 impl Plugin for LevelPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(setup);
+        app.add_startup_system(setup).add_plugin(SkyboxPlugin);
     }
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(SceneBundle {
-        scene: asset_server.load("models/level.gltf#Scene0"),
-        ..default()
-    }).insert(Name::new("Level"));
+    commands
+        .spawn(SceneBundle {
+            scene: asset_server.load("models/level.gltf#Scene0"),
+            ..default()
+        })
+        .insert(Name::new("Level"));
 }
